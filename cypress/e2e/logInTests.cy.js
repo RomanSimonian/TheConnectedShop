dfa5c2fd675b2b358786cc8acf4dbf070b73d424
RSimonianLaflei <wwpranik@gmail.com>
2024-04-10 10:24:33 +0300

    log in page test created

--- cypress/e2e/logInTests.cy.js
+++ cypress/e2e/logInTests.cy.js
@@ -1,5 +1,4 @@
 import {LogInPage} from "../support/pageObjects/logInPage";
-
 describe('LogIn testing', () => {
     const loginPage = new LogInPage();
     it('should log in', () => {
@@ -10,7 +9,28 @@ describe('LogIn testing', () => {
 
             cy.url().should('include', loginPage.loginPageUrl);
         })
-
     });
 
+    // it('Login with incorrect credentials', () => {
+    //     cy.visit(loginPage.loginPageUrl);
+    //     cy.get(loginPage.logInButton)
+    //         .first()
+    //         .click();
+    //     cy.on('window:alert', (str) => {
+    //       expect(str).to.equal('Заповнцть це поле.');
+    //       cy.wrap(str).as('alert');
+    //     });
+    // });
+
+    // it('Login with incorrect credentials', () => {
+    //     const expectedAlert = {
+    //       'en-US': 'Invalid username or password',
+    //       'ru-RU': 'Неправильный логин или пароль',
+    //     };
+
+    //     cy.on('window:alert', (str) => {
+    //       expect(str).to.equal(expectedAlert[Cypress.config('language')]);
+    //       cy.wrap(str).as('alert');
+    //     });
+    // });
 });
